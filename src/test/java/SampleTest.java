5d1bb313231b54683ae62ed74e1e518de2399172
priya <priyab8@gmail.com>
2018-08-16 21:20:04 +0530

    junit demo

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void simpleTest() {
+		int a=1;
+		int b=2;
+		assertEquals(3,a + b);
+	}
+	@Test
+	public void simpleTestFail() {
+		int a=1;
+		int b=2;
+		assertEquals(6,a + b);
+	}
+}
